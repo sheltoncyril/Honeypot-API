c34847f744925d982e712e980529204180339837
sheltoncyril <sheltoncyril@gmail.com>
2020-12-07 21:25:16 +0530

    changed user containers to honeypots

--- api/models.py
+++ api/models.py
@@ -20,7 +20,7 @@ class Honeypot(models.Model):
     template = models.ForeignKey(HoneypotTemplate, on_delete=models.CASCADE, related_name='template')
     container_id = models.CharField(max_length=100, blank=True, null=True)
     container_ip = models.GenericIPAddressField(blank=True,null=True)
-    user = models.ForeignKey(User,on_delete=models.CASCADE, related_name='containers', default="None")
+    user = models.ForeignKey(User,on_delete=models.CASCADE, related_name='honeypots', default="None")
 
     def __str__(self):
         return f'{self.name} - {self.container_id}'
